bfb122031a359761cb12660059f646b7a6de672e
i.dovhyi <i.dovhyi@abtollc.com>
2022-05-20 16:22:53 +0300

    rename component

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentsComponent } from './components/components/components.component';
 import { InterfacesComponent } from './interfaces/interfaces/interfaces.component';
 import { ServicesComponent } from './services/services/services.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    ComponentsComponent,
     InterfacesComponent,
-    ServicesComponent
+    ServicesComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule
