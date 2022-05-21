1f300561d469bd6cbc0b0f9edcdbbd57e630a8b7
i.dovhyi <i.dovhyi@abtollc.com>
2022-05-21 18:12:42 +0300

    task ANG-41

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InterfacesComponent } from './interfaces/interfaces/interfaces.component';
 import { ServicesComponent } from './services/services/services.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
     InterfacesComponent,
-    ServicesComponent,
-    DashboardComponent
+    ServicesComponent
   ],
   imports: [
     CommonModule
